19bf2cef72586efdafa784079a7a20e11a2c15db
Ronen Margolin <roni948576@gmail.com>
2023-10-02 13:40:16 +0300

    first screen design added

--- adminClient/src/gui/scene/main/MainScene.java
+++ adminClient/src/gui/scene/main/MainScene.java
@@ -1,11 +1,17 @@
 package gui.scene.main;
 
+import gui.scene.management.ManagementScene;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class MainScene{
     @FXML
@@ -15,32 +21,56 @@ public class MainScene{
     @FXML
     private BorderPane centerStage;
     @FXML
-    private Button DetailsButton;
+    private Button managementButton;
     @FXML
-    private Button newExecutionButton;
+    private Button allocationsButton;
     @FXML
-    private Button resultsButton;
+    private Button executionHistoryButton;
+
+    private static MainScene mainController = null;
 
     @FXML
     public void initialize(){
-        DetailsButton.setOnAction(this::handleDetailsButton);
-        newExecutionButton.setOnAction(this::handleNewExecutionButton);
-        resultsButton.setOnAction(this::handleResultsButton);
+        managementButton.setOnAction(this::handleManagementButton);
+        allocationsButton.setOnAction(this::handleAllocationsButton);
+        executionHistoryButton.setOnAction(this::handleExecutionHistoryButton);
         content.prefWidthProperty().bind(Bindings.max(900, mainRoot.widthProperty().subtract(20)));
         content.prefHeightProperty().bind(Bindings.max(600, mainRoot.heightProperty().subtract(20)));
+        new Thread(() -> getInstance(this)).start();
+        Platform.runLater(()->managementButton.fire());
+    }
+
+    public static synchronized MainScene getInstance(MainScene mainScene) {
+        if(mainController==null){
+            mainController = mainScene;
+        }
+        return mainScene;
     }
 
     @FXML
-    private void handleDetailsButton(ActionEvent actionEvent) {
+    private void handleManagementButton(ActionEvent actionEvent) {
+        loadSubScene("managementScene.fxml", ManagementScene.class);
     }
 
     @FXML
-    public void handleNewExecutionButton(ActionEvent actionEvent){
+    public void handleAllocationsButton(ActionEvent actionEvent){
 
     }
 
     @FXML
-    public void handleResultsButton(ActionEvent actionEvent){
+    public void handleExecutionHistoryButton(ActionEvent actionEvent){
 
+    }
+
+    private void loadSubScene(String fileName, Class<?> loaderClass)
+    {
+        try {
+            FXMLLoader loader = new FXMLLoader(loaderClass.getResource(fileName));
+            centerStage.setCenter(null);
+            Parent root = loader.load();
+            centerStage.setCenter(root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ adminClient/src/gui/scene/management/ManagementScene.java
@@ -0,0 +1,4 @@
+package gui.scene.management;
+
+public class ManagementScene {
+}
