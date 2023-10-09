d5c6ef973d881fc6d2da6693d36ffccb5d1e2b99
Ronen Margolin <roni948576@gmail.com>
2023-10-09 23:32:03 +0300

    links work well

--- adminClient/src/gui/scene/management/ManagementScene.java
+++ adminClient/src/gui/scene/management/ManagementScene.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
@@ -53,6 +54,36 @@ public class ManagementScene {
                 filePathHolder.setText(newValue.getHyperlink());
             }
         });
+        filePathHolder.setOnAction(this::handleHyperlinkClick);
+    }
+
+    private void handleHyperlinkClick(ActionEvent actionEvent) {
+        String filePath = filePathHolder.getText();
+        new Thread(() -> {
+            // Check if Desktop is supported (i.e., the application is running in a GUI environment)
+            if (Desktop.isDesktopSupported()) {
+                Desktop desktop = Desktop.getDesktop();
+                File fileToOpen = new File(filePath);
+
+                try {
+                    // Check if the file/folder exists before attempting to open it
+                    if (fileToOpen.exists()) {
+                        if (desktop.isSupported(Desktop.Action.OPEN)) {
+                            desktop.open(fileToOpen);
+                        } else {
+                            System.err.println("Desktop is not supported.");
+                        }
+                    } else {
+                        System.out.println("File or folder does not exist: " + filePath);
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error opening file/folder: " + filePath);
+                    e.printStackTrace(System.err);
+                }
+            } else {
+                System.err.println("Desktop is not supported.");
+            }
+        }).start();
     }
 
     private void handleSetThreadCountButton(ActionEvent actionEvent) {
--- adminClient/src/gui/scene/management/worldNameItem/WorldNameItem.java
+++ adminClient/src/gui/scene/management/worldNameItem/WorldNameItem.java
@@ -26,35 +26,6 @@ public class WorldNameItem {
         return name;
     }
 
-    private void handleHyperlinkClick(ActionEvent actionEvent) {
-        String filePath = getHyperlink();
-        new Thread(() -> {
-            // Check if Desktop is supported (i.e., the application is running in a GUI environment)
-            if (Desktop.isDesktopSupported()) {
-                Desktop desktop = Desktop.getDesktop();
-                File fileToOpen = new File(filePath);
-
-                try {
-                    // Check if the file/folder exists before attempting to open it
-                    if (fileToOpen.exists()) {
-                        if (desktop.isSupported(Desktop.Action.OPEN)) {
-                            desktop.open(fileToOpen);
-                        } else {
-                            System.err.println("Desktop is not supported.");
-                        }
-                    } else {
-                        System.out.println("File or folder does not exist: " + filePath);
-                    }
-                } catch (IOException e) {
-                    System.err.println("Error opening file/folder: " + filePath);
-                    e.printStackTrace(System.err);
-                }
-            } else {
-                System.err.println("Desktop is not supported.");
-            }
-        }).start();
-    }
-
     @Override
     public String toString() {
         return name;
